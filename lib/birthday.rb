e10767334c8d354e22cb36cfdd4baec1d132a551
Payton Gray <paytongray18@gmail.com>
2020-08-15 17:41:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts"Happy BIrthday #{kids_name}! You are now#{age} years old!"
+  end
 end

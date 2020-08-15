1f638952758e8b827c7ca84ebabb595c1adbdfad
Payton Gray <paytongray18@gmail.com>
2020-08-15 17:27:42 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@
 # }
 
 def select_winner(passengers)
-  passengers.
+  passengers.each do |suite,name|
+    if suite == :suite_a && name.start_with?(A)
+      winner = name
+    end
+  end
 end
+
+winner
